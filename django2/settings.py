2a4a94588121123189002b9c008af3998e984aae
Joao Paulo Nunes <jpnune@gmail.com>
2025-08-26 13:52:11 -0300

    deploy projeto com sucesso!

--- django2/settings.py
+++ django2/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-@u@nd9e&r%+824vmq4hsignv8jlkwx9t=^lw++(fho#_b0a_d7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
